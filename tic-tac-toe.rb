fe4e17ebfdc160c8000a9f56cd4dc07129d84961
EvanOliver0 <ewoliver@protonmail.com>
2019-11-02 23:34:27 -0400

    Finish game functionality

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,6 +1,15 @@
 class GameBoard
-  attr_reader :spaces, :victor
+  attr_reader :victor
   @@CODES = {UL: 0, UC: 1, UR: 2, CL: 3, CC: 4, CR: 5, LL: 6, LC: 7, LR: 8}
+  @@COMBOS = {UL: [[1, 2], [3, 6], [4, 8]],
+              UC: [[0, 2], [4, 7]],
+              UR: [[0, 1], [4, 6], [5, 8]],
+              CL: [[0, 6], [4, 5]],
+              CC: [[0, 8], [1, 7], [2, 6], [3, 5]],
+              CR: [[2, 8], [3, 4]],
+              LL: [[0, 3], [2, 4], [7, 8]],
+              LC: [[1, 4], [6, 8]],
+              LR: [[0, 4], [2, 5], [6, 7]]}
 
   def initialize
     @spaces = [nil] * 9
@@ -11,12 +20,10 @@ class GameBoard
     return !@victor.nil?
   end
 
-  def place_X(space_code)
-    set_space(space_code, "X")
-  end
-
-  def place_O(space_code)
-    set_space(space_code, "O")
+  def place_marker(space_code, marker)
+    valid = set_space(space_code, marker)
+    check_for_victor(space_code, marker) if valid
+    valid
   end
 
   def to_s
@@ -24,7 +31,7 @@ class GameBoard
     @spaces.each do |space|
       markers.push(space.nil? ? " " : space)
     end
-    
+
     "\n #{markers[0]} | #{markers[1]} | #{markers[2]}\n" \
     "-----------\n" \
     " #{markers[3]} | #{markers[4]} | #{markers[5]}\n" \
@@ -33,16 +40,60 @@ class GameBoard
   end
 
   private
+  def check_for_victor(last_space, marker)
+    if full?
+      @victor = "draw"
+      return true
+    end
+
+    last_space_code = last_space.upcase.to_sym
+    if @@CODES[last_space_code].nil?
+      puts "Invalid space: #{last_space}"
+      return false
+    end
+
+    last_index = decode_space(last_space)
+    if @spaces[last_index] != marker
+      puts "Space #{last_space} doesn't have an #{marker}... " \
+           "Something's gone wrong."
+      return false
+    end
+
+    possible_combos = @@COMBOS[last_space_code]
+    possible_combos.each do |combo|
+      winning_combo = true
+
+      combo.each do |index|
+        winning_combo = false unless @spaces[index] == marker
+      end
+
+      if winning_combo
+        @victor = marker
+        return true
+      end
+
+    end
+    return false
+  end
 
   def decode_space(space_code)
     @@CODES[space_code.upcase.to_sym]
   end
 
-  def set_space(space_code, marker)
-    index = decode_space(space_code)
+  def full?
+    !@spaces.include? nil
+  end
 
+  def set_space(space_code, marker)
+    unless @victor.nil?
+      puts "There's no point in placing another marker; " \
+           "#{@victor} has already won!"
+      return false
+    end
+
+    index = decode_space(space_code)
     if index.nil?
-      puts "Invalid input"
+      puts "Invalid input: #{space_code}"
       return false
     end
 
@@ -56,10 +107,36 @@ class GameBoard
   end
 end
 
-board = GameBoard.new
-board.place_X("CC")
-puts board
-board.place_O("CC")
-puts board
-board.place_O("UR")
-puts board
+def play_game
+  puts "Time for a game of tic-tac-toe!"
+  puts "Use U, C, L to specify row and L, C, R to specify column, "
+  puts "as in 'UR' for the upper right space or 'CC' for the center space.\n\n"
+
+  board = GameBoard.new
+  x_turn = true
+
+  while !board.game_over?
+    marker = x_turn ? "X" : "O"
+    print "#{marker}'s move: "
+    space = gets.chomp.upcase
+
+    success = board.place_marker(space, marker)
+    if success
+      puts "#{marker} took space #{space}!"
+      puts board
+      x_turn = !x_turn
+    else
+      puts "Something went wrong - try again.\n\n"
+    end
+  end
+
+  return board.victor
+end
+
+play = true
+while play
+  victor = play_game
+  puts(victor == "draw" ? "Cat's game!" : "#{victor} won!")
+  print "Play again (y/N)? "
+  play = (gets.chomp.downcase == "y")
+end
